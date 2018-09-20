c274be15ba6729d3f5afca75bdeae3d7fd5d1b57
liga12 <Ligalauz12>
2018-09-20 17:50:27 +0300

    add controller integration test

---
+++ user-service/src/test/java/com/booking/user/controller/TicketControllerIntegrationTest.java
@@ -0,0 +1,30 @@
+package com.booking.user.controller;
+
+import com.booking.user.persistence.repository.UserRepository;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+public class TicketControllerIntegrationTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+        userRepository.deleteAll();
+    }
+
+}
